6c162b78910026e7ca7ed26fddb4436189ff6cea
wuyang910217 <wuyang910217@gmail.com>
2016-05-08 22:21:03 +0800

    添加侧边栏

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Radium, { StyleRoot } from 'radium';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from  'material-ui/AppBar';
 
 import NavBar from './shared/NavBar.jsx';
+import AppDrawer from './shared/AppDrawer.jsx';
 
 class App extends Component {
   getChildContext(){
@@ -10,7 +12,13 @@ class App extends Component {
       muiTheme: getMuiTheme()
     };
   }
-
+  componentWillMount() {
+    let setNarbarState = () => {
+      this.setState({renderNavNar: window.innerWidth > 700});
+    };
+    setNarbarState();
+    window.onresize = setNarbarState;
+  }
   getStyles() {
     return {
       root: {
@@ -27,11 +35,16 @@ class App extends Component {
     return (
       <StyleRoot>
         <div style={ styles.root }>
-          <NavBar />
+          { this.state.renderNavNar ? <NavBar /> : <AppBar onLeftIconButtonTouchTap={this.handleTouchTap.bind(this)} /> }
+          <AppDrawer ref='drawer' />
           { this.props.children }
         </div>
       </StyleRoot>
     );
+  }
+
+  handleTouchTap(){
+    this.refs.drawer.handleToggle();
   }
 }
 
---
+++ imports/ui/shared/AppDrawer.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Drawer from 'material-ui/Drawer';
+import { List, ListItem, MakeSelectable } from 'material-ui/List';
+const SelectableList = MakeSelectable(List);
+
+class AppDrawer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      listIndex: ''
+    };
+  }
+
+    componentDidMount() {
+    this.setState({
+      listIndex: this.getSelectedIndex()
+    })
+  }
+  componentWillReceiveProps() {
+    this.setState({
+      listIndex: this.getSelectedIndex()
+    })
+  }
+  getSelectedIndex() {
+    return this.context.router.isActive('/', true) ? '/' :
+      this.context.router.isActive('/signup') ? '/signup' :
+      this.context.router.isActive('/login') ? '/login' : '';
+  }
+
+  render() {
+    let styles = {
+      header: {
+        fontSize: '24px',
+        color: '#fff',
+        lineHeight: '64px',
+        fontWeight: '300',
+        backgroundColor: '#00bcd4',
+        paddingLeft: '24px',
+        paddingTop: '0px',
+        marginBottom: '8px',
+      },
+      selectedList: {
+        color: '#ff4081',
+        backgroundColor: 'rgba(0, 0, 0, 0.03)',
+      }
+    };
+    return (
+      <Drawer open={this.state.open}
+               docked={false}
+               onRequestChange={this.handleRequestChange.bind(this)}>
+        <div style={styles.header}>
+          Chat Room Demo
+        </div>
+        <SelectableList
+          selectedItemStyle={styles.selectedList}
+          value={this.state.listIndex}
+          onChange={this.handleChange.bind(this)}>
+          <ListItem value='/' primaryText='Home' />
+          <ListItem value='/signup' primaryText='Sign up' />
+          <ListItem value='/login' primaryText='Log in' />
+        </SelectableList>
+      </Drawer>
+    );
+  }
+
+  handleChange(e, index) {
+    this.context.router.push(index);
+    this.setState({
+      open: false,
+      listIndex: index,
+    });
+  }
+
+  handleRequestChange(open) {
+    this.setState({open: open});
+  }
+  handleToggle() {
+    this.setState({open: !this.state.open});
+  }
+}
+
+AppDrawer.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
+
+export default AppDrawer;
